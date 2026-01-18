346c860473ec1513f0905d6b4a4fad7c1b7c4cc5
tomekkak <117871036+10m452@users.noreply.github.com>
2026-01-18 19:41:17 +0100

    Przerobione dane, skrypt z pythona do importu (częściowy)
    pliki CSV zawierają przerobione i gotowe do importu dane, a skrypt import_danych.py umożliwia wgranie ich do lokalnej bazy danych
    
    plik tabele_sql.txt zawiera strukturę (lekko zmodyfikowaną w stosunku do oryginalnych jakie robiliśmy) tabel jakich użyłem do importu
    
    pliki countries_list i genres_list zawierają listy krajów i gatunków jakie są w tabelach

---
+++ import_danych.py
@@ -0,0 +1,109 @@
+import csv
+import psycopg2
+
+
+
+# połączenie z bazą
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    database="baza", # baza
+    user="uzytkownik",
+    password="haslo" # haslo
+)
+
+cur = conn.cursor()
+
+# import do tabeli movies
+with open("movies_data.csv", encoding="cp1250") as f:
+
+    reader = csv.reader(f)
+    next(reader)  # jeśli plik ma nagłówek
+    for row in reader:
+        # Zamiana 'NA' na None
+        row = [None if (x == 'NA' or x == '') else x for x in row]
+        cur.execute(
+            "INSERT INTO movies (movie_id, title, year_of_production, time, description) VALUES (%s, %s, %s, %s, %s)",
+            row[:5]  # upewnij się, że bierzesz tylko tyle kolumn, ile jest w tabeli
+        )
+
+conn.commit()
+
+# import do tabeli people
+with open("people_data.csv", encoding="cp1250") as f:
+
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        # Zamiana 'NA' i '' na None (NULL)
+        row = [None if (x == 'NA' or x == '') else x for x in row]
+        cur.execute(
+            "INSERT INTO people (person_id, name) VALUES (%s, %s)",
+            row[:2]
+        )
+        
+conn.commit()
+
+# import do tabeli countries
+with open("countries_data.csv", encoding="cp1250") as f:
+
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        # Zamiana 'NA' i '' na None (NULL)
+        row = [None if (x == 'NA' or x == '') else x for x in row]
+        cur.execute(
+            "INSERT INTO countries (movie_id, country) VALUES (%s, %s)",
+            row[:2]
+        )
+
+conn.commit()
+
+# import do tabeli genres
+with open("genres_data.csv", encoding="cp1250") as f:
+
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        # Zamiana 'NA' i '' na None (NULL)
+        row = [None if (x == 'NA' or x == '') else x for x in row]
+        cur.execute(
+            "INSERT INTO genres (movie_id, genre) VALUES (%s, %s)",
+            row[:2]
+        )
+
+conn.commit()
+
+
+# import do tabeli people_movies (aktorzy)
+with open("movies_actors_data.csv", encoding="cp1250") as f:
+
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        # Zamiana 'NA' i '' na None (NULL)
+        row = [None if (x == 'NA' or x == '') else x for x in row]
+        cur.execute(
+            "INSERT INTO people_movies (movie_id, person_id, role) VALUES (%s, %s, %s)",
+            row[:2] + row[3:]
+        )
+
+conn.commit()
+
+# import do tabeli people_movies (tworcy)
+with open("movies_crew_data.csv", encoding="cp1250") as f:
+
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        # Zamiana 'NA' i '' na None (NULL)
+        row = [None if (x == 'NA' or x == '') else x for x in row]
+        cur.execute(
+            "INSERT INTO people_movies (movie_id, person_id, role) VALUES (%s, %s, %s)",
+            row[:2] + row[3:]
+        )
+
+conn.commit()
+
+cur.close()
+conn.close()
